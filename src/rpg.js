692bda5d1567bfedfac6fc32ccd226ca7f9dc8c5
silvermistshadow <bmead72@gmail.com>
2019-08-23 14:30:53 -0700

    work on battle system

--- src/rpg.js
+++ src/rpg.js
@@ -8,8 +8,14 @@ export class Rpg {
 }
 
 export class battle extends Rpg {
-  constructor() {
+  constructor(character, enemy) {
     super();
+    this.character = character;
+    this.enemy = enemy;
+  }
+  /*Okay, this one is going to be a bit complicated. We somehow need to get the stats from both of the previous things:
+  the character and the enemy.*/
+  playerAttackRoller() {
   }
 }
 
