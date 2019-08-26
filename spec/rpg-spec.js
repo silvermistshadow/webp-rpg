7ba09d206fb9d9b133c8bb3c0339e9c8dc5f527d
silvermistshadow <bmead72@gmail.com>
2019-08-26 13:57:35 -0700

    work on rolls and damage calculations

--- spec/rpg-spec.js
+++ spec/rpg-spec.js
@@ -1,6 +1,6 @@
 import { Character } from "./../src/Character";
 import { Stats } from "./../src/Stats";
-import { Rpg } from './../src/Rpg';
+import { Rpg, battle } from './../src/Rpg';
 import { getRandomIntInclusive } from './../src/getRandomIntInclusive';
 
 describe('Character', function() {
@@ -20,4 +20,46 @@ describe('Character', function() {
     console.log(enemyMaker);
   });
 
+});
+
+describe('Rpg', function() {
+  it('should take a Character object and store it', function() {
+    let exampleStats = new Stats(12, 10, 8);
+    let exampleChar = new Character("Cecil", "Waterbender", exampleStats);
+    let newRpg = new Rpg(exampleChar);
+    console.log(newRpg);
+  });
+});
+
+describe('battle', function() {
+  it('should take character and enemy data and store it', function() {
+    let exampleStats = new Stats(12, 10, 8);
+    let exampleChar = new Character("Cecil", "Waterbender", exampleStats);
+    let exampleEnemyStats = new Stats(12, 10, 8);
+    exampleEnemyStats.enemyStatMaker();
+    let enemyMaker = new Character("Enemy", "Firebender", exampleEnemyStats);
+    let exampleBattle = new battle(exampleChar, enemyMaker);
+    console.log(exampleBattle);
+  });
+  it('should roll a number and add it to the character stats', function() {
+    let exampleStats = new Stats(12, 10, 8);
+    let exampleChar = new Character("Cecil", "Waterbender", exampleStats);
+    let exampleEnemyStats = new Stats(12, 10, 8);
+    exampleEnemyStats.enemyStatMaker();
+    console.log(exampleChar.stats.chi);
+    let enemyMaker = new Character("Enemy", "Firebender", exampleEnemyStats);
+    let exampleBattle = new battle(exampleChar, enemyMaker);
+    console.log(exampleBattle.playerAttackRoller());
+  });
+  it('should then take the rolled number and subtract the enemys defense', function(){
+    let exampleStats = new Stats(12, 10, 8);
+    let exampleChar = new Character("Cecil", "Waterbender", exampleStats);
+    let exampleEnemyStats = new Stats(12, 10, 8);
+    exampleEnemyStats.enemyStatMaker();
+    let enemyMaker = new Character("Enemy", "Firebender", exampleEnemyStats);
+    let exampleBattle = new battle(exampleChar, enemyMaker);
+    let exampleDamage = exampleBattle.playerDamageEnemy();
+    console.log(exampleDamage);
+
+  });
 });
--- src/rpg.js
+++ src/rpg.js
@@ -1,5 +1,6 @@
 import { getRandomIntInclusive } from "./getRandomIntInclusive";
 import { Stats } from "./Stats";
+import { Character } from "./Character";
 
 export class Rpg {
   constructor(character) {
@@ -13,9 +14,33 @@ export class battle extends Rpg {
     this.character = character;
     this.enemy = enemy;
   }
-  /*Okay, this one is going to be a bit complicated. We somehow need to get the stats from both of the previous things:
-  the character and the enemy.*/
+
+
   playerAttackRoller() {
+    let rolledNum = getRandomIntInclusive(1, 20);
+    let modifiedNum = rolledNum + this.character.stats.chi;
+    return modifiedNum;
   }
+
+  enemyAttackRoller() {
+    let rolledNum = getRandomIntInclusive(1, 20);
+    let modifiedNum = rolledNum + this.enemy.stats.chi;
+    return modifiedNum;
+  }
+
+  playerDamageEnemy() {
+    let initDamage = this.playerAttackRoller();
+    let modifiedDamage = initDamage - this.enemy.stats.resistance;
+    return modifiedDamage;
+  }
+
+  enemyDamagePlayer() {
+    let initDamage = this.enemyAttackRoller();
+    let modifiedDamage = initDamage - this.character.stats.resistance;
+    return modifiedDamage;
+  }
+
+  
+
 }
 
